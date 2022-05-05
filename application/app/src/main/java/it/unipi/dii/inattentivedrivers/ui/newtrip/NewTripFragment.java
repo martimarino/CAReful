91558bd675e4d35b86171fb69dad00925fb92720
mcasini <mirko.casini97@gmail.com>
2022-05-05 11:37:20 +0200

    Class names updated

--- application/app/src/main/java/it/unipi/dii/inattentivedrivers/ui/newtrip/NewTripFragment.java
+++ application/app/src/main/java/it/unipi/dii/inattentivedrivers/ui/newtrip/NewTripFragment.java
@@ -1,4 +1,4 @@
-package it.unipi.dii.inattentivedrivers.ui.home;
+package it.unipi.dii.inattentivedrivers.ui.newtrip;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import it.unipi.dii.inattentivedrivers.databinding.FragmentHomeBinding;
+import it.unipi.dii.inattentivedrivers.databinding.FragmentNewtripBinding;
 
-public class HomeFragment extends Fragment {
+public class NewTripFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentNewtripBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        NewTripViewModel newTripViewModel =
+                new ViewModelProvider(this).get(NewTripViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentNewtripBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textNewtrip;
+        newTripViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- application/app/src/main/java/it/unipi/dii/inattentivedrivers/ui/newtrip/NewTripViewModel.java
+++ application/app/src/main/java/it/unipi/dii/inattentivedrivers/ui/newtrip/NewTripViewModel.java
@@ -1,16 +1,16 @@
-package it.unipi.dii.inattentivedrivers.ui.home;
+package it.unipi.dii.inattentivedrivers.ui.newtrip;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class NewTripViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public NewTripViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is New Trip fragment");
     }
 
     public LiveData<String> getText() {
