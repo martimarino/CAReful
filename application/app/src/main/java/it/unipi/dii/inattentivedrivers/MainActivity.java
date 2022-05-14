fb91946a13720b6db6e5e1ca7824002cabbe3d70
martimarino <martina.marino1996@gmail.com>
2022-05-14 15:59:46 +0200

    Cosmetic changes.

--- application/app/src/main/java/it/unipi/dii/inattentivedrivers/MainActivity.java
+++ application/app/src/main/java/it/unipi/dii/inattentivedrivers/MainActivity.java
@@ -28,6 +28,6 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-    }  // end onCreate method
+    }
 
 }
